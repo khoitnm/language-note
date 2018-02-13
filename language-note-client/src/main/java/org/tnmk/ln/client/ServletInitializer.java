6498c6336e50bcb76ea8a1a79028e05a39a09902
Khoi Tran <khoi.tnm@gmail.com>
2018-02-12 23:04:46 -0500

    The language-note-client now can load all static files (css, js...) and running fine with both standalone and web (deployed on Tomcat) modes.

---
+++ language-note-client/src/main/java/org/tnmk/ln/client/ServletInitializer.java
@@ -0,0 +1,19 @@
+package org.tnmk.ln.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.tnmk.common.infrastructure.projectprofile.ProjectProfileUtil;
+
+/**
+ * We will need this class when changing this project package from 'jar' to 'war'
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServletInitializer.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.profiles(ProjectProfileUtil.getDefaultProfile()).sources(SpringBootWebApplication.class);
+    }
+}
