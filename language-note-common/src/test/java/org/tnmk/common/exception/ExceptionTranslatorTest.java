f781c37bf308b7870c4ac5b3533b0951230e36da
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2018-03-08 16:16:18 -0500

    Add UnitTest: ExceptionTranslator

--- language-note-common/src/test/java/org/tnmk/common/exception/ExceptionTranslatorTest.java
+++ language-note-common/src/test/java/org/tnmk/common/exception/ExceptionTranslatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.MethodParameter;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.MapBindingResult;
@@ -13,6 +14,8 @@ import org.tnmk.common.exception.filter.ExceptionTranslator;
 import org.tnmk.common.exception.model.error.ErrorResult;
 import org.tnmk.common.infrastructure.validator.BeanValidator;
 import org.tnmk.common.testingmodel.Person;
+import org.tnmk.common.testingmodel.PersonFactory;
+import org.tnmk.common.testingmodel.Pet;
 import org.tnmk.common.testingmodel.service.MockBusinessLogic;
 
 import java.lang.reflect.Method;
@@ -26,15 +29,15 @@ public class ExceptionTranslatorTest {
     private ExceptionTranslator exceptionTranslator = new ExceptionTranslator(new BeanValidationExceptionTranslator());
 
     @Test
-    public void beanValidationException(){
+    public void beanValidationException() {
         try {
             BeanValidator beanValidator = new BeanValidator();
             Person person = new Person();
-            person.setDob(LocalDate.of(2001,01,01));
-            person.setComingOfAgeDate(LocalDate.of(2000,01,01));
-            person.setEmails(Arrays.asList("aaa@gmail.com","",null,"xyz","bbb@gmail.com"));
+            person.setDob(LocalDate.of(2001, 01, 01));
+            person.setComingOfAgeDate(LocalDate.of(2000, 01, 01));
+            person.setEmails(Arrays.asList("aaa@gmail.com", "", null, "xyz", "bbb@gmail.com"));
             beanValidator.validate(person);
-        }catch (BeanValidationException ex){
+        } catch (BeanValidationException ex) {
             ErrorResult errorResult = exceptionTranslator.processValidationError(ex);
             Assert.assertNotNull(errorResult.getCode());
             Assert.assertNotNull(errorResult.getFieldErrors().get(0).getField());
@@ -48,17 +51,17 @@ public class ExceptionTranslatorTest {
     }
 
     @Test
-    public void methodArgumentNotValidException(){
+    public void methodArgumentNotValidException() {
         try {
             //Mock BindingResult with Total 2 errors
             MapBindingResult bindingResult = new MapBindingResult(new HashMap<>(), "targetObjectName");
-            bindingResult.rejectValue("field1","errorCode1");
-            bindingResult.reject("errorCode2",new Object[]{"errorArg2.1",2.2},"defaultMessage2");
+            bindingResult.rejectValue("field1", "errorCode1");
+            bindingResult.reject("errorCode2", new Object[]{"errorArg2.1", 2.2}, "defaultMessage2");
 
             Method method = MockBusinessLogic.class.getMethods()[0];
             MethodParameter methodParameter = new MethodParameter(method, 0, 1);
             throw new MethodArgumentNotValidException(methodParameter, bindingResult);
-        }catch (MethodArgumentNotValidException ex){
+        } catch (MethodArgumentNotValidException ex) {
             ErrorResult errorResult = exceptionTranslator.processValidationError(ex);
             Assert.assertNotNull(errorResult.getCode());
             Assert.assertNotNull(errorResult.getFieldErrors().get(0).getField());
@@ -67,10 +70,33 @@ public class ExceptionTranslatorTest {
     }
 
     @Test
-    public void unexpectedException(){
+    public void processBadRequestException() {
+        try {
+            throw new TypeMismatchException(PersonFactory.createJasonBourne(), Pet.class);
+        } catch (TypeMismatchException ex) {
+            ErrorResult errorResult = exceptionTranslator.processBadRequestException(ex);
+            Assert.assertNotNull(errorResult.getCode());
+            Assert.assertNotNull(errorResult.getUserMessage());
+        }
+    }
+
+    @Test
+    public void processAuthenticationException() {
+        String rootMessage = "Authentication exception message";
+        try {
+            throw new AuthenticationException(rootMessage);
+        } catch (AuthenticationException ex) {
+            ErrorResult errorResult = exceptionTranslator.processUnauthenticationException(ex);
+            Assert.assertNotNull(errorResult.getCode());
+            Assert.assertTrue(errorResult.getUserMessage().toLowerCase().contains(rootMessage.toLowerCase()));
+        }
+    }
+
+    @Test
+    public void unexpectedException() {
         try {
             throw new UnexpectedException("Some unexpected exception");
-        }catch (BaseException ex){
+        } catch (BaseException ex) {
             ErrorResult errorResult = exceptionTranslator.processInternalBaseException(ex);
             Assert.assertNotNull(errorResult.getCode());
             Assert.assertNotNull(errorResult.getUserMessage());
@@ -78,11 +104,11 @@ public class ExceptionTranslatorTest {
     }
 
     @Test
-    public void nullPointerException(){
+    public void nullPointerException() {
         try {
             String a = null;
             a.toLowerCase();
-        }catch (NullPointerException ex){
+        } catch (NullPointerException ex) {
             ErrorResult errorResult = exceptionTranslator.processUnknownInternalException(ex);
             Assert.assertNotNull(errorResult.getCode());
             Assert.assertNotNull(errorResult.getUserMessage());
